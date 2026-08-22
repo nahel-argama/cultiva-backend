5102ce4ec20eaa18107198d9b538041d187169c4
henriqueoz <pedroozorio30@gmail.com>
2026-08-22 15:24:31 -0300

    feat(database): update signup migration

--- database/migrations/2026_08_16_185552_create_signup_tables.php
+++ database/migrations/2026_08_16_185552_create_signup_tables.php
@@ -10,18 +10,27 @@ return new class extends Migration {
      */
     public function up(): void
     {
+        Schema::table('users', function (Blueprint $table) {
+            $table->timestamp('last_login')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->softDeletes();
+        });
+
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
             $table->string('neighborhood', 30);
             $table->string('street', 50);
+            $table->string('number', 10);
             $table->string('city', 30);
             $table->string('state', 2);
             $table->string('zip', 10);
             $table->string('complement', 50)->nullable();
+            $table->geography('coordinate', subtype: 'point', srid: 4326)->nullable();
         });
 
         Schema::create('retailers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
             $table->foreignId('address_id')->constrained('addresses');
             $table->string('identity', 14)->unique();
             $table->string('phone', 15);
@@ -29,17 +38,10 @@ return new class extends Migration {
 
         Schema::create('producers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
             $table->foreignId('address_id')->constrained('addresses');
             $table->string('identity', 14)->unique();
             $table->string('phone', 15);
-        });
-
-        Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('last_login')->nullable();
-            $table->foreignId('retailer_id')->nullable()->constrained('retailers');
-            $table->foreignId('producer_id')->nullable()->constrained('producers');
-            $table->boolean('is_active')->default(true);
-            $table->softDeletes();
         });
     }
 };
