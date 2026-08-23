1a96f6eb56f13714d44d75ac4599b54b3070af5d
henriqueoz <pedroozorio30@gmail.com>
2026-08-22 22:40:38 -0300

    feat(database): update signup migration

--- database/migrations/2026_08_16_185552_create_signup_tables.php
+++ database/migrations/2026_08_16_185552_create_signup_tables.php
@@ -11,37 +11,54 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->timestamp('last_login')->nullable();
-            $table->boolean('is_active')->default(true);
-            $table->softDeletes();
+            $table->timestamp('last_login')->nullable()->after('remember_token');
+            $table->boolean('is_active')->default(true)->after('last_login');
+            $table->softDeletes()->after('updated_at');
         });
 
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->string('neighborhood', 30);
-            $table->string('street', 50);
-            $table->string('number', 10);
-            $table->string('city', 30);
-            $table->string('state', 2);
-            $table->string('zip', 10);
-            $table->string('complement', 50)->nullable();
-            $table->geography('coordinate', subtype: 'point', srid: 4326)->nullable();
-        });
+            $table->morphs('addressable');
 
-        Schema::create('retailers', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('address_id')->constrained('addresses');
-            $table->string('identity', 14)->unique();
-            $table->string('phone', 15);
+            $table->string('zip', 8);
+            $table->string('street', 100);
+            $table->string('number', 20);
+            $table->string('complement', 50)->nullable();
+            $table->string('reference_point', 150)->nullable();
+            $table->string('neighborhood', 50);
+            $table->string('city', 50);
+            $table->string('state', 2);
+            $table->geography('coordinate', subtype: 'point', srid: 4326)->nullable();
+
+            $table->timestamps();
         });
 
         Schema::create('producers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users');
-            $table->foreignId('address_id')->constrained('addresses');
-            $table->string('identity', 14)->unique();
+            $table->foreignId('user_id')->unique()->constrained('users')->cascadeOnDelete();
+
+            $table->boolean('is_company')->default(false);
+            $table->string('document_number', 14)->unique();
+            $table->string('trade_name', 100);
+            $table->string('legal_name', 100)->nullable();
             $table->string('phone', 15);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('retailers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->unique()->constrained('users')->cascadeOnDelete();
+
+            $table->string('document_number', 14)->unique();
+            $table->string('trade_name', 100);
+            $table->string('legal_name', 100)->nullable();
+            $table->string('business_type', 30)->nullable();
+            $table->string('phone', 15);
+
+            $table->timestamps();
+            $table->softDeletes();
         });
     }
 };
