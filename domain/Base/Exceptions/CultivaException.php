d408bd23d16ceaf8372eeee9dceb185c91af4c0c
ngracioli <nicolas.gracioli@gmail.com>
2026-08-31 20:20:17 -0300

    feat(exception): implement custom CultivaException with status code and context handling

--- domain/Base/Exceptions/CultivaException.php
+++ domain/Base/Exceptions/CultivaException.php
@@ -2,6 +2,24 @@
 
 namespace Cultiva\Base\Exceptions;
 
+use Illuminate\Http\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
-final class CultivaException extends HttpException {}
+final class CultivaException extends HttpException
+{
+
+    public function __construct(
+        private int $statusCode,
+        string $message = '',
+        ?\Throwable $previous = null,
+        private array $context = [],
+    ) {}
+
+    public function render(): JsonResponse
+    {
+        return response()->json([
+            'message' => $this->getMessage(),
+            'context' => $this->context,
+        ], $this->getStatusCode());
+    }
+}
