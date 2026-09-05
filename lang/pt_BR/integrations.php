bb467b73288d90978165f513d2b6ddd715a94cc8
ngracioli <nicolas.gracioli@gmail.com>
2026-09-05 17:16:39 -0300

    feat(i18n): add product-source messages

--- lang/pt_BR/integrations.php
+++ lang/pt_BR/integrations.php
@@ -4,4 +4,8 @@ return [
     'geo' => [
         'failed_to_search' => 'Falha ao buscar informações geográficas.',
     ],
+    'product_source' => [
+        'not_found' => 'Produto não encontrado no catálogo externo.',
+        'unavailable' => 'Catálogo de produtos indisponível.',
+    ],
 ];
