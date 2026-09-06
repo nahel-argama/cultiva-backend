b635b881730dfc6d2ea49ff93a5e9ce544d57059
henriqueoz <pedroozorio30@gmail.com>
2026-09-06 02:56:49 -0300

    feat(auth): add signup metadata endpoint with localized enum labels

---
+++ lang/pt_BR/enums.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'profile_type' => [
+        'producer' => 'Produtor',
+        'retailer' => 'Varejista',
+        'delivery' => 'Entregador',
+    ],
+    'activity_segment' => [
+        'vegetables' => 'Hortaliças',
+        'fruits' => 'Frutas',
+        'tubers_roots' => 'Tubérculos e Raízes',
+        'herbs_spices' => 'Ervas e Especiarias',
+        'grains' => 'Grãos',
+        'specialty' => 'Especialidades',
+    ],
+    'business_type' => [
+        'supermarket' => 'Supermercado',
+        'hortifruti' => 'Hortifruti',
+        'restaurant' => 'Restaurante',
+    ],
+    'cnh_category' => [
+        'A' => 'Categoria A',
+        'B' => 'Categoria B',
+        'C' => 'Categoria C',
+        'D' => 'Categoria D',
+        'E' => 'Categoria E',
+        'AB' => 'Categoria AB',
+        'AC' => 'Categoria AC',
+        'AD' => 'Categoria AD',
+        'AE' => 'Categoria AE',
+    ],
+    'cargo_type' => [
+        'dry' => 'Carga Seca',
+        'climate_controlled' => 'Climatizada',
+        'refrigerated' => 'Refrigerada',
+    ],
+];
